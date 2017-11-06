d71023ff06836c29c046f8612f4428518ffeccfd
刘君 <liujun@xianjinxia.com>
2017-11-06 10:17:36 +0800

    my first springboot web demo

---
+++ src/main/java/com/clivial/web/refactor/RefactorWebApplication.java
@@ -0,0 +1,12 @@
+package com.clivial.web.refactor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RefactorWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RefactorWebApplication.class, args);
+	}
+}
---
+++ src/main/java/com/clivial/web/refactor/demo/UserController.java
@@ -0,0 +1,16 @@
+package com.clivial.web.refactor.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by liquan on 2017/11/6.
+ */
+@RestController
+public class UserController {
+
+    @RequestMapping("/hello")
+    public String hello(){
+        return "Hello,my first refator of casshman web test!";
+    }
+}
