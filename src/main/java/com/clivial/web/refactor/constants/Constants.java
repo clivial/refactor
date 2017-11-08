665a42d3a250f20374d6470f4ee024729f504124
刘君 <liujun@xianjinxia.com>
2017-11-08 11:19:18 +0800

    添加缺失文件

---
+++ src/main/java/com/clivial/web/refactor/constants/Constants.java
@@ -0,0 +1,10 @@
+package com.clivial.web.refactor.constants;
+
+/**
+ * Created by liquan on 2017/11/7.
+ */
+public class Constants {
+    // Spring profile used to disable swagger
+    public static final String SPRING_PROFILE_NO_SWAGGER = "no-swagger";
+
+}
